8dfa85473c939d5d07a30e90c5128f4482d1bb6b
charleswick <charles_wick25@milton.edu>
2024-05-26 10:34:25 -0400

    got all resulting boards to print

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -146,6 +146,19 @@ public class MyPlayer {
 
 
                         }
+                        while(a==1&&b==1&&c==0){
+                            b=b-1;
+                            System.out.println(a + " " + " " + b + " " + (c));
+
+
+                        }
+                        while(a==2&&b==0&&c==0){
+                            a=a-1;
+                            System.out.println(a + " " + " " + b + " " + (c));
+
+
+                        }
+
 
 
 
